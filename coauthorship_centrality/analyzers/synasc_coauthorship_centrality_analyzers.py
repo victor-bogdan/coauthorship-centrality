7899d164667a9554c454bb14255182ba71d412b3
Victor_Bogdan <victor199704@gmail.com>
2022-06-25 15:28:53 +0300

    Added yearly analysis resource files

--- coauthorship_centrality/analyzers/synasc_coauthorship_centrality_analyzers.py
+++ coauthorship_centrality/analyzers/synasc_coauthorship_centrality_analyzers.py
@@ -9,42 +9,32 @@ class SYNASCCoauthorshipCentralityAnalyzer(CoauthorshipCentralityAnalyzer):
 
     def __init__(self):
         super().__init__()
-        self.__init_data()
 
-    def __init_data(self):
-        project_root_path = dirname(dirname(__file__))
-        synasc_data_path = "{0}/resources/synasc_data".format(project_root_path)
-        folder_path = "{0}/*.json".format(synasc_data_path)
-        file_paths = [f for f in glob(folder_path)]
+    def get_coauthorship_centrality(self, centrality_measure, analysis_type, end_year, year_period_type):
+        self.__init_data(year_period_type)
 
-        for file_path in file_paths:
-            with open(file_path, "r", encoding="utf-8-sig") as file:
-                data = loads(file.read())
-                self._yearly_accumulative_analysis_data[str(data['end_year'])] = data
-
-    def get_coauthorship_centrality(self, centrality_measure, analysis_type, end_year):
         centrality_dict = {}
 
         if analysis_type == "author":
             for author_name, author_data in \
                     self._yearly_accumulative_analysis_data[end_year]['accumulative_authors'].items():
-                if centrality_measure.lower() == "betweenness":
-                    centrality_dict[author_name] = author_data['accumulative_author_betweenness_centrality']
-                elif centrality_measure.lower() == "closeness":
-                    centrality_dict[author_name] = author_data['accumulative_author_closeness_centrality']
+                if centrality_measure.lower() == "closeness":
+                    centrality_dict[author_name] = author_data['year_author_closeness_centrality']
+                elif centrality_measure.lower() == "betweenness":
+                    centrality_dict[author_name] = author_data['year_author_betweenness_centrality']
                 elif centrality_measure.lower() == "degree":
-                    centrality_dict[author_name] = author_data['accumulative_author_degree_centrality']
+                    centrality_dict[author_name] = author_data['year_author_degree_centrality']
                 elif centrality_measure.lower() == "pagerank":
-                    centrality_dict[author_name] = author_data['accumulative_author_pagerank_centrality']
+                    centrality_dict[author_name] = author_data['year_author_pagerank_centrality']
         elif analysis_type == "collaborator_group":
             for collaborator_group_id, collaborator_group_data in \
                     self._yearly_accumulative_analysis_data[end_year]['accumulative_collaborator_groups'].items():
-                if centrality_measure.lower() == "betweenness":
-                    centrality_dict[collaborator_group_id] = \
-                        collaborator_group_data['accumulative_collaborator_groups_betweenness_centrality']
-                elif centrality_measure.lower() == "closeness":
+                if centrality_measure.lower() == "closeness":
                     centrality_dict[collaborator_group_id] = \
                         collaborator_group_data['accumulative_collaborator_groups_closeness_centrality']
+                elif centrality_measure.lower() == "betweenness":
+                    centrality_dict[collaborator_group_id] = \
+                        collaborator_group_data['accumulative_collaborator_groups_betweenness_centrality']
                 elif centrality_measure.lower() == "degree":
                     centrality_dict[collaborator_group_id] = \
                         collaborator_group_data['accumulative_collaborator_groups_degree_centrality']
@@ -53,6 +43,21 @@ class SYNASCCoauthorshipCentralityAnalyzer(CoauthorshipCentralityAnalyzer):
                         collaborator_group_data['accumulative_collaborator_groups_pagerank_centrality']
 
         return centrality_dict
+
+    def __init_data(self, year_period_type):
+        project_root_path = dirname(dirname(__file__))
+        if year_period_type == "yearly":
+            synasc_data_path = "{0}/resources/synasc_data/yearly".format(project_root_path)
+        else:
+            synasc_data_path = "{0}/resources/synasc_data/yearly_accumulative".format(project_root_path)
+
+        folder_path = "{0}/*.json".format(synasc_data_path)
+        file_paths = [f for f in glob(folder_path)]
+
+        for file_path in file_paths:
+            with open(file_path, "r", encoding="utf-8-sig") as file:
+                data = loads(file.read())
+                self._yearly_accumulative_analysis_data[str(data['end_year'])] = data
 
     def get_node_edges(self, analysis_type, end_year):
         if analysis_type == "author":

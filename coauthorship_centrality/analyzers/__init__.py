c069fca9fa72b47c22fa15d9cbdf482c0bd9d2af
Victor_Bogdan <victor199704@gmail.com>
2022-06-28 18:58:46 +0300

    Refactored centrality package

--- coauthorship_centrality/analyzers/__init__.py
+++ coauthorship_centrality/analyzers/__init__.py
@@ -1,2 +1,1 @@
-from .coauthorship_centrality_analyzers import CoauthorshipCentralityAnalyzer, CENTRALITY_MEASURES, YEAR_PERIOD_TYPES, \
-    LAYER_NODE_TYPES
+from .coauthorship_centrality_analyzers import CoauthorshipCentralityAnalyzer, CENTRALITY_MEASURES, LAYER_NODE_TYPES
--- coauthorship_centrality/analyzers/coauthorship_centrality_analyzers.py
+++ coauthorship_centrality/analyzers/coauthorship_centrality_analyzers.py
@@ -1,27 +1,31 @@
-from coauthorship_centrality.core.centrality.yearly_accumulative_centrality import compute_yearly_accumulative_graph_centrality
-from coauthorship_centrality.core.centrality.yearly_centrality import compute_yearly_graph_centrality
+from ..core.centrality.yearly_accumulative_centrality import \
+    compute_yearly_accumulative_graph_centrality
+from ..core.centrality.year_centrality import compute_year_graph_centrality
 
 CENTRALITY_MEASURES = ["closeness", "betweenness", "degree", "pagerank"]
-YEAR_PERIOD_TYPES = ["yearly", "yearly_accumulative"]
 LAYER_NODE_TYPES = ["default", "group"]
 
 
 class CoauthorshipCentralityAnalyzer:
 
-    def get_coauthorship_centrality(
+    def get_year_coauthorship_centrality(
+            self,
+            year_graph_data,
+            centrality_measure,
+            layer_node_type
+    ):
+        yearly_analysis_data = compute_year_graph_centrality(year_graph_data, centrality_measure, layer_node_type)
+        return yearly_analysis_data
+
+    def get_yearly_accumulative_coauthorship_centrality(
             self,
             yearly_graph_data,
             centrality_measure,
-            year_period_type,
             layer_node_type
     ):
-        if year_period_type == "yearly":
-            yearly_analysis_data = compute_yearly_graph_centrality(yearly_graph_data, centrality_measure)
-            return yearly_analysis_data
-        else:
-            yearly_accumulative_analysis_data = compute_yearly_accumulative_graph_centrality(
-                yearly_graph_data, centrality_measure, layer_node_type)
-            return yearly_accumulative_analysis_data
+        yearly_accumulative_analysis_data = compute_yearly_accumulative_graph_centrality(
+            yearly_graph_data, centrality_measure, layer_node_type)
+        return yearly_accumulative_analysis_data
 
 
 class DBLPCoauthorshipCentralityAnalyzer:

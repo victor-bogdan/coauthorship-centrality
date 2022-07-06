3ad9016c19b373a5a1c8d0bcc8f3f691e0e562f5
Victor_Bogdan <victor199704@gmail.com>
2022-07-06 18:16:49 +0300

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='coauthorship-centrality',
-    version='1.2.0',
+    version='1.2.1',
     packages=find_packages(exclude=['internal']),
     url='',
     license='Apache 2.0',

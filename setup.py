1ec12f77916aa07bee3b42849c547ab89de0fa4d
Victor Bogdan <victor199704@gmail.com>
2023-11-09 12:47:18 +0200

    Updated project version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='coauthorship-centrality',
-    version='1.4.0',
+    version='1.5.0',
     packages=find_packages(exclude=['internal']),
     url='',
-    license='Apache 2.0',
+    license='MIT',
     author='Victor',
     author_email='victor199704@gmail.com',
     description='A package used for computing node centrality in coauthorship networks.',
